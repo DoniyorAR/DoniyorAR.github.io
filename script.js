da570355f973a75b28f8a2c876d9d21c3db011b9
DoniyorAR <84126471+DoniyorAR@users.noreply.github.com>
2024-11-19 19:05:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ function submitForm() {
     const classNumber = document.getElementById('classNumber').value.trim();
     
     if (name && classNumber) {
-        // Save user data to localStorage
         localStorage.setItem('userName', name);
         localStorage.setItem('userClassNumber', classNumber);
-        // Redirect to the question page
-        window.location.href = 'question.html';
+        window.location.href = 'question.html';  // Redirects to question.html
     } else {
         alert("Ism va sinf raqamini to'liq kiriting!");
     }
