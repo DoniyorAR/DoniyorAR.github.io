729e4caac9226124c4ebc5310b9d388f25ab99d3
DoniyorAR <84126471+DoniyorAR@users.noreply.github.com>
2024-11-19 19:08:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function submitForm() {
+    console.log("submitForm function is called"); // Debug log
     const name = document.getElementById('name').value.trim();
     const classNumber = document.getElementById('classNumber').value.trim();
     
     if (name && classNumber) {
         localStorage.setItem('userName', name);
         localStorage.setItem('userClassNumber', classNumber);
-        window.location.href = 'question.html';  // Redirects to question.html
+        console.log("Redirecting to question.html"); // Debug log
+        window.location.href = 'question.html';
     } else {
         alert("Ism va sinf raqamini to'liq kiriting!");
     }
