cc4af01cfc30e81aeab3c4afb3bfec5ff4f2a9cb
DoniyorAR <84126471+DoniyorAR@users.noreply.github.com>
2024-11-20 20:06:24 +0900

    Update questionScript.js

--- questionScript.js
+++ questionScript.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function loadQuestions() {
-    Papa.parse("adjusted_quiz_questions.csv", {
+    Papa.parse("final_quiz_questions.csv", {
         download: true,
         header: true,
         skipEmptyLines: true,
